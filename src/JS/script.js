a2108801c56bdf8e95bc10d23befc75d2c8cf648
umeshnarayancedcoss <umeshnarayan@cedcoss.com>
2022-09-30 17:41:24 +0530

    encryption

---
+++ src/JS/script.js
@@ -0,0 +1,19 @@
+const arr_text = [];
+function fun() {
+
+    var text = document.getElementById('text').value;
+
+    const arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+    arr_text.push(text);
+    //console.log(arr_text);
+    var i = 0;
+    arr_text.forEach(element => {
+        arr_text[i]; {
+            i++;
+        }
+        console.log(arr_text);
+    });
+
+
+}
